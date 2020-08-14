d2b10ab09865ad165d8c1c19712580aa9999cb97
Suraj <sakshatkatyarmal234@gmail.com>
2020-08-14 18:47:27 +0530

    stateful changes made

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,39 @@ import 'package:tesseract_ocr/tesseract_ocr.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+  
+ 
+class _MyAppState extends State<MyApp> {
+  
+  // ignore: override_on_non_overriding_member
   final picker = new ImagePicker();
 
   File _image;
   String _extractText = 'Unknown';
+  
   Future pickimage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    final  pickedFile = await picker.getImage(source: ImageSource.gallery);
+  
 
     _image = File(pickedFile.path);
+    final String _extrtxt = await TesseractOcr.extractText(pickedFile.path,language:"eng");
 
-    _extractText =
-        await TesseractOcr.extractText(pickedFile.path, language: "eng");
+    // _extractText =
+    //     await TesseractOcr.extractText(pickedFile.path, language: "eng");
+        setState(() {
+           _extractText = _extrtxt;
+        });
 
     log(_image.path);
     log(_extractText);
-
   }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,6 +52,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            
             children: <Widget>[
               ClipOval(
                 child: Material(
@@ -55,20 +71,24 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              Text(_image?.path == null ? "Add Photo" : _image?.path,
+              Text("Add Photo",
                   style: TextStyle(
                       color: Colors.grey[800],
                       fontWeight: FontWeight.bold,
                       fontSize: 40)),
-              Center(
-                child: _image == null
-                    ? Text('No image selected.')
-                    : Image.file(_image),
-              ),
+              // Center(
+              //   child: _image == null
+              //       ? Text('No image selected.')
+              //       : Image.file(_image),
+              // ),
+              
+              Center(child: Text(_extractText))
             ],
           ),
         ),
       ),
     );
   }
+
+ 
 }
